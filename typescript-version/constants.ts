add1322cb061625314180c9d2abb791bb82e0c9b
cosmic_perott <142322366+cosmic-perott@users.noreply.github.com>
2026-05-28 12:10:00 +0900

    Delete typescript-version directory

--- typescript-version/constants.ts
+++
@@ -1,7 +0,0 @@
-import { Suggestion } from './types.ts';
-
-export const SUGGESTIONS: Suggestion[] = [
-  { id: 's1', text: 'Plan an NYC trip' },
-  { id: 's2', text: 'Honest Tokyo spots' },
-  { id: 's3', text: 'Avoid Paris tourist traps' },
-];
